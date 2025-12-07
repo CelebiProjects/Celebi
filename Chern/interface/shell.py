c7ca618812c8648e2e94444a51f1549a46d932a1
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2025-12-07 13:46:59 +0800

    use current_project_path indead of c

--- Chern/interface/shell.py
+++ Chern/interface/shell.py
@@ -8,6 +8,7 @@ import os
 import subprocess
 
 from ..utils import csys
+from ..utils.message import Message
 from ..kernel.vobject import VObject
 from ..interface.ChernManager import get_manager
 from ..kernel.vtask import create_task
@@ -25,13 +26,13 @@ MANAGER = get_manager()
 def cd_project(line: str) -> None:
     """Switch to a different project and change directory to its path."""
     MANAGER.switch_project(line)
-    os.chdir(MANAGER.c.path)
+    os.chdir(MANAGER.current_object().path)
 
 
 def shell_cd_project(line: str) -> None:
     """Switch to a different project and print the new path."""
     cd_project(line)
-    print(MANAGER.c.path)
+    print(MANAGER.current_object().path)
 
 
 def cd(line: str) -> None:
@@ -48,26 +49,26 @@ def cd(line: str) -> None:
 
 def _cd_by_index(index: int) -> None:
     """Change directory by numeric index."""
-    sub_objects = MANAGER.c.sub_objects()
-    successors = MANAGER.c.successors()
-    predecessors = MANAGER.c.predecessors()
+    sub_objects = MANAGER.current_object().sub_objects()
+    successors = MANAGER.current_object().successors()
+    predecessors = MANAGER.current_object().predecessors()
     total = len(sub_objects)
 
     if index < total:
         sub_objects.sort(key=lambda x: (x.object_type(), x.path))
-        cd(MANAGER.c.relative_path(sub_objects[index].path))
+        cd(MANAGER.current_object().relative_path(sub_objects[index].path))
         return
 
     index -= total
     total = len(predecessors)
     if index < total:
-        cd(MANAGER.c.relative_path(predecessors[index].path))
+        cd(MANAGER.current_object().relative_path(predecessors[index].path))
         return
 
     index -= total
     total = len(successors)
     if index < total:
-        cd(MANAGER.c.relative_path(successors[index].path))
+        cd(MANAGER.current_object().relative_path(successors[index].path))
         return
 
     color_print("Out of index", "remind")
@@ -179,8 +180,8 @@ def cp(source: str, destination: str) -> None:
     cp SOURCE DIRECTORY
     """
     # Within a task or algorithm, use object-specific copy
-    if MANAGER.c.object_type() in ("task", "algorithm"):
-        MANAGER.c.cp(source, destination)
+    if MANAGER.current_object().object_type() in ("task", "algorithm"):
+        MANAGER.current_object().cp(source, destination)
         return
 
     # Normalize paths
@@ -202,20 +203,19 @@ def cp(source: str, destination: str) -> None:
         print(result.colored())
 
 
-def ls(_: str) -> None:
+def ls(_: str):
     """List the contents of the current object."""
-    print("Running")
-    MANAGER.current_object().ls()
+    return MANAGER.current_object().ls()
 
 
 def short_ls(_: str) -> None:
     """Show short listing of the current object."""
-    MANAGER.c.short_ls()
+    MANAGER.current_object().short_ls()
 
 
 def mkalgorithm(obj: str, use_template: bool = False) -> None:
     """Create a new algorithm."""
-    line = csys.refine_path(obj, MANAGER.c.path)
+    line = csys.refine_path(obj, MANAGER.current_object().path)
     parent_path = os.path.abspath(line+"/..")
     object_type = VObject(parent_path).object_type()
     if object_type not in ("directory", "project"):
@@ -226,7 +226,7 @@ def mkalgorithm(obj: str, use_template: bool = False) -> None:
 
 def mktask(line: str) -> None:
     """Create a new task."""
-    line = csys.refine_path(line, MANAGER.c.path)
+    line = csys.refine_path(line, MANAGER.current_object().path)
     parent_path = os.path.abspath(line+"/..")
     object_type = VObject(parent_path).object_type()
     if object_type not in ("directory", "project"):
@@ -237,7 +237,7 @@ def mktask(line: str) -> None:
 
 def mkdata(line: str) -> None:
     """Create a new data task."""
-    line = csys.refine_path(line, MANAGER.c.path)
+    line = csys.refine_path(line, MANAGER.current_object().path)
     parent_path = os.path.abspath(line+"/..")
     object_type = VObject(parent_path).object_type()
     if object_type not in ("directory", "project"):
@@ -248,7 +248,7 @@ def mkdata(line: str) -> None:
 
 def mkdir(line: str) -> None:
     """Create a new directory."""
-    line = csys.refine_path(line, MANAGER.c.path)
+    line = csys.refine_path(line, MANAGER.current_object().path)
     parent_path = os.path.abspath(line+"/..")
     object_type = VObject(parent_path).object_type()
     if object_type not in ("directory", "project"):
@@ -277,56 +277,56 @@ def rm(line: str) -> None:
 
 def rm_file(file_name: str) -> None:
     """Remove a file from current task or algorithm."""
-    if MANAGER.c.object_type() not in ("task", "algorithm"):
+    if MANAGER.current_object().object_type() not in ("task", "algorithm"):
         print("Unable to call rm_file if you are not in a task or algorithm.")
         return
     # Deal with * case
     if file_name == "*":
-        path = MANAGER.c.path
+        path = MANAGER.current_object().path
         for current_file in os.listdir(path):
             # protect .chern and chern.yaml
             if current_file in (".chern", "chern.yaml"):
                 continue
-            result = MANAGER.c.rm_file(current_file)
+            result = MANAGER.current_object().rm_file(current_file)
             if result.messages:  # If there are error messages
                 print(result.colored())
         return
-    result = MANAGER.c.rm_file(file_name)
+    result = MANAGER.current_object().rm_file(file_name)
     if result.messages:  # If there are error messages
         print(result.colored())
 
 
 def mv_file(file_name: str, dest_file: str) -> None:
     """Move a file within current task or algorithm."""
-    if MANAGER.c.object_type() not in ("task", "algorithm"):
+    if MANAGER.current_object().object_type() not in ("task", "algorithm"):
         print("Unable to call mv_file if you are not in a task or algorithm.")
         return
-    result = MANAGER.c.move_file(file_name, dest_file)
+    result = MANAGER.current_object().move_file(file_name, dest_file)
     if result.messages:  # If there are error messages
         print(result.colored())
 
 
 def add_source(line: str) -> None:
     """Add a source to the current object."""
-    MANAGER.c.add_source(line)
+    MANAGER.current_object().add_source(line)
 
 
 def jobs(_: str) -> None:
     """Show jobs for current algorithm or task."""
-    object_type = MANAGER.c.object_type()
+    object_type = MANAGER.current_object().object_type()
     if object_type not in ("algorithm", "task"):
         print("Not able to found job")
         return
-    MANAGER.c.jobs()
+    MANAGER.current_object().jobs()
 
 
-def status() -> None:
+def status():
     """Show status of current object."""
-    print(MANAGER.current_object().printed_status().colored())
+    return MANAGER.current_object().printed_status()
 
 def import_file(filename: str) -> None:
     """Import a file into current task or algorithm."""
-    if MANAGER.c.object_type() not in ("task", "algorithm"):
+    if MANAGER.current_object().object_type() not in ("task", "algorithm"):
         print("Unable to call importfile if you are not in a task or algorithm.")
         return
 
@@ -338,12 +338,12 @@ def import_file(filename: str) -> None:
             return
         for file in os.listdir(filename):
             print(f"Importing: from {os.path.join(filename, file)}")
-            print(f"Importing: to {MANAGER.c.path}")
-            result = MANAGER.c.import_file(os.path.join(filename, file))
+            print(f"Importing: to {MANAGER.current_object().path}")
+            result = MANAGER.current_object().import_file(os.path.join(filename, file))
             if result.messages:  # If there are error messages
                 print(result.colored())
         return
-    result = MANAGER.c.import_file(filename)
+    result = MANAGER.current_object().import_file(filename)
     if result.messages:  # If there are error messages
         print(result.colored())
 
@@ -351,20 +351,20 @@ def import_file(filename: str) -> None:
 # pylint: disable=too-many-branches
 def add_input(path: str, alias: str) -> None:
     """Add an input to current task or algorithm."""
-    if MANAGER.c.object_type() == "directory":
-        destination_path = MANAGER.c.relative_path(path)
-        dest_obj = VObject(os.path.join(MANAGER.c.path, destination_path))
+    if MANAGER.current_object().object_type() == "directory":
+        destination_path = MANAGER.current_object().relative_path(path)
+        dest_obj = VObject(os.path.join(MANAGER.current_object().path, destination_path))
         if dest_obj.object_type() == "task":
-            sub_objects = MANAGER.c.sub_objects()
+            sub_objects = MANAGER.current_object().sub_objects()
             for obj in sub_objects:
                 if obj.object_type() != "task":
                     continue
-                obj_path = MANAGER.c.relative_path(obj.path)
+                obj_path = MANAGER.current_object().relative_path(obj.path)
                 task = MANAGER.sub_object(obj_path)
                 task.add_input(path, alias)
         elif dest_obj.object_type() == "directory":
             dest_sub_objects = dest_obj.sub_objects()
-            sub_objects = MANAGER.c.sub_objects()
+            sub_objects = MANAGER.current_object().sub_objects()
             if len(dest_sub_objects) != len(sub_objects):
                 print("The number of sub-objects does not match.")
                 return
@@ -388,54 +388,54 @@ def add_input(path: str, alias: str) -> None:
                 if obj.path.split("_")[-1] != dest_obj.path.split("_")[-1]:
                     print("The sub-objects are not aligned.")
                     return
-                obj_path = MANAGER.c.relative_path(obj.path)
+                obj_path = MANAGER.current_object().relative_path(obj.path)
                 task = MANAGER.sub_object(obj_path)
                 task.add_input(dest_obj.path, alias)
                 if length > 100 and int(dest_obj.path.split("_")[-1]) % (length // 10) == 0:
                     print(f"Progress: {int(dest_obj.path.split('_')[-1])}/{length}")
         return
-    if MANAGER.c.object_type() not in ("task", "algorithm"):
+    if MANAGER.current_object().object_type() not in ("task", "algorithm"):
         print("Unable to call add_input if you are not in a task or algorithm.")
         return
-    MANAGER.c.add_input(path, alias)
+    MANAGER.current_object().add_input(path, alias)
 
 
 def add_algorithm(path: str) -> None:
     """Add an algorithm to current task."""
-    if MANAGER.c.object_type() == "directory":
-        sub_objects = MANAGER.c.sub_objects()
+    if MANAGER.current_object().object_type() == "directory":
+        sub_objects = MANAGER.current_object().sub_objects()
         for obj in sub_objects:
             if obj.object_type() != "task":
                 continue
-            obj_path = MANAGER.c.relative_path(obj.path)
+            obj_path = MANAGER.current_object().relative_path(obj.path)
             task = MANAGER.sub_object(obj_path)
             task.add_algorithm(path)
         return
-    if MANAGER.c.object_type() != "task":
+    if MANAGER.current_object().object_type() != "task":
         print("Unable to call add_algorithm if you are not in a task.")
         return
-    MANAGER.c.add_algorithm(path)
+    MANAGER.current_object().add_algorithm(path)
 
 
 def add_parameter(par: str, value: str) -> None:
     """Add a parameter to current task."""
-    if MANAGER.c.object_type() == "directory":
-        sub_objects = MANAGER.c.sub_objects()
+    if MANAGER.current_object().object_type() == "directory":
+        sub_objects = MANAGER.current_object().sub_objects()
         for obj in sub_objects:
             if obj.object_type() != "task":
                 continue
-            obj_path = MANAGER.c.relative_path(obj.path)
+            obj_path = MANAGER.current_object().relative_path(obj.path)
             task = MANAGER.sub_object(obj_path)
             task.add_parameter(par, value)
         return
-    if MANAGER.c.object_type() != "task":
+    if MANAGER.current_object().object_type() != "task":
         print("Unable to call add_input if you are not in a task.")
         return
-    MANAGER.c.add_parameter(par, value)
+    MANAGER.current_object().add_parameter(par, value)
 
 def add_parameter_subtask(dirname: str, par: str, value: str) -> None:
     """Add a parameter to current task."""
-    if MANAGER.c.object_type() not in ("directory", "project"):
+    if MANAGER.current_object().object_type() not in ("directory", "project"):
         print("Unable to call add_parameter_subtask if you are not in a dir")
         return
     obj = MANAGER.sub_object(dirname)
@@ -446,61 +446,61 @@ def add_parameter_subtask(dirname: str, par: str, value: str) -> None:
 
 def set_environment(env: str) -> None:
     """Set environment for current task."""
-    if MANAGER.c.object_type() == "directory":
-        sub_objects = MANAGER.c.sub_objects()
+    if MANAGER.current_object().object_type() == "directory":
+        sub_objects = MANAGER.current_object().sub_objects()
         for obj in sub_objects:
             if obj.object_type() != "task":
                 continue
-            obj_path = MANAGER.c.relative_path(obj.path)
+            obj_path = MANAGER.current_object().relative_path(obj.path)
             task = MANAGER.sub_object(obj_path)
             task.set_environment(env)
         return
-    if MANAGER.c.object_type() != "task":
+    if MANAGER.current_object().object_type() != "task":
         print("Unable to call set_environment if you are not in a task.")
         return
-    MANAGER.c.set_environment(env)
+    MANAGER.current_object().set_environment(env)
 
 
 def set_memory_limit(limit: str) -> None:
     """Set memory limit for current task."""
-    if MANAGER.c.object_type() == "directory":
-        sub_objects = MANAGER.c.sub_objects()
+    if MANAGER.current_object().object_type() == "directory":
+        sub_objects = MANAGER.current_object().sub_objects()
         for obj in sub_objects:
             if obj.object_type() != "task":
                 continue
-            obj_path = MANAGER.c.relative_path(obj.path)
+            obj_path = MANAGER.current_object().relative_path(obj.path)
             task = MANAGER.sub_object(obj_path)
             task.set_memory_limit(limit)
         return
-    if MANAGER.c.object_type() != "task":
+    if MANAGER.current_object().object_type() != "task":
         print("Unable to call set_memory_limit if you are not in a task.")
         return
-    MANAGER.c.set_memory_limit(limit)
+    MANAGER.current_object().set_memory_limit(limit)
 
 
 def rm_parameter(par: str) -> None:
     """Remove a parameter from current task."""
-    if MANAGER.c.object_type() != "task":
+    if MANAGER.current_object().object_type() != "task":
         print("Unable to call add_input if you are not in a task.")
         return
-    MANAGER.c.remove_parameter(par)
+    MANAGER.current_object().remove_parameter(par)
 
 
 def remove_input(alias: str) -> None:
     """Remove an input from current task or algorithm."""
-    if MANAGER.c.object_type() == "directory":
-        sub_objects = MANAGER.c.sub_objects()
+    if MANAGER.current_object().object_type() == "directory":
+        sub_objects = MANAGER.current_object().sub_objects()
         for obj in sub_objects:
             if obj.object_type() != "task":
                 continue
-            obj_path = MANAGER.c.relative_path(obj.path)
+            obj_path = MANAGER.current_object().relative_path(obj.path)
             task = MANAGER.sub_object(obj_path)
             task.remove_input(alias)
         return
-    if not MANAGER.c.is_task_or_algorithm():
+    if not MANAGER.current_object().is_task_or_algorithm():
         print("Unable to call remove_input if you are not in a task.")
         return
-    MANAGER.c.remove_input(alias)
+    MANAGER.current_object().remove_input(alias)
 
 
 def add_host(host: str, url: str) -> None:
@@ -558,18 +558,18 @@ def remove_runner(runner: str) -> None:
 
 def send(path: str) -> None:
     """Send a path to current object."""
-    MANAGER.c.send(path)
+    MANAGER.current_object().send(path)
 
 
 def submit(runner: str = "local") -> None:
     """Submit to the runner."""
-    message = MANAGER.c.submit(runner)
+    message = MANAGER.current_object().submit(runner)
     print(message.colored())
 
 
 def view(browser: str = "open") -> None:
     """View impressions for current task."""
-    is_task = MANAGER.c.is_task()
+    is_task = MANAGER.current_object().is_task()
     if not is_task:
         print("Not able to view")
         return
@@ -582,21 +582,21 @@ def edit_script(obj: str) -> None:
     path = os.path.join(os.environ["HOME"], ".chern", "config.yaml")
     yaml_file = metadata.YamlFile(path)
     editor = yaml_file.read_variable("editor", "vi")
-    subprocess.call([editor, f"{MANAGER.c.path}/{obj}"])
+    subprocess.call([editor, f"{MANAGER.current_object().path}/{obj}"])
 
 
 def config() -> None:
     """Edit configuration for current task or algorithm."""
-    if not MANAGER.c.is_task_or_algorithm():
+    if not MANAGER.current_object().is_task_or_algorithm():
         print("Not able to config")
         return
     path = os.path.join(os.environ["HOME"], ".chern", "config.yaml")
     yaml_file = metadata.YamlFile(path)
     editor = yaml_file.read_variable("editor", "vi")
     # Generate a template file if the config file does not exist
-    if not os.path.exists(f"{MANAGER.c.path}/chern.yaml"):
-        with open(f"{MANAGER.c.path}/chern.yaml", "w", encoding="utf-8") as f:
-            if MANAGER.c.object_type() == "task":
+    if not os.path.exists(f"{MANAGER.current_object().path}/chern.yaml"):
+        with open(f"{MANAGER.current_object().path}/chern.yaml", "w", encoding="utf-8") as f:
+            if MANAGER.current_object().object_type() == "task":
                 f.write("""environment: chern
 memory_limit: 256Mi
 alias:
@@ -606,34 +606,38 @@ parameters: {{}}""")
                 f.write("""environment: script
 commands:
   - echo 'Hello, world!'""")
-    subprocess.call([editor, f"{MANAGER.c.path}/chern.yaml"])
+    subprocess.call([editor, f"{MANAGER.current_object().path}/chern.yaml"])
 
 def danger_call(cmd: str) -> None:
     """Execute a dangerous command and print the result."""
-    message = MANAGER.c.danger_call(cmd)
+    message = MANAGER.current_object().danger_call(cmd)
     print(message.colored())
 
 
 def workaround_preshell() -> tuple[bool, str]:
     """Execute pre-shell workaround for the current task."""
-    if not MANAGER.c.is_task():
+    if not MANAGER.current_object().is_task():
         return (False, "Not able to call workaround if you are not in a task.")
-    return MANAGER.c.workaround_preshell()
+    return MANAGER.current_object().workaround_preshell()
 
 
 def workaround_postshell() -> None:
     """Execute post-shell workaround for the current task."""
-    if not MANAGER.c.is_task():
+    if not MANAGER.current_object().is_task():
         print("Not able to call workaround if you are not in a task.")
         return
-    MANAGER.c.workaround_postshell("")
+    MANAGER.current_object().workaround_postshell("")
 
 
 def trace(impression: str) -> None:
     """Trace back to the task or algorithm that generated the impression."""
-    MANAGER.c.trace(impression)
+    MANAGER.current_object().trace(impression)
 
 
-def history() -> None:
+def history() -> Message:
     """Print the history of a task or algorithm."""
-    MANAGER.c.history()
+    return MANAGER.current_object().history()
+
+def changes() -> Message:
+    """Print the changes of a task or algorithm."""
+    return MANAGER.current_object().changes()
