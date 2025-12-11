5136d1410c1627537902f9401a1d906814157e5e
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2025-12-11 22:59:11 +0800

    sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 celebi-navigate() {
 workdir="$(python - <<'EOF'
-from Chern.interface.ChernManager import ChernProjectManager
+from CelebiChrono.interface.ChernManager import ChernProjectManager
 manager = ChernProjectManager().get_manager()
 project_name=manager.get_current_project()
 print(manager.get_project_path(project_name))
@@ -11,21 +11,21 @@ cd $workdir
 
 celebi-ls() {
 python - <<'EOF'
-from Chern.interface.shell import ls
+from CelebiChrono.interface.shell import ls
 print(ls("").colored())
 EOF
 }
 
 celebi-status() {
 python - <<'EOF'
-from Chern.interface.shell import status
+from CelebiChrono.interface.shell import status
 print(status().colored())
 EOF
 }
 
 celebi-cd() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import cd
+from CelebiChrono.interface.shell import cd
 import sys
 cd(sys.argv[1])
 EOF
@@ -33,7 +33,7 @@ EOF
 
 celebi-cdproject() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import shell_cd_project
+from CelebiChrono.interface.shell import shell_cd_project
 import sys
 shell_cd_project(sys.argv[1])
 EOF
@@ -41,7 +41,7 @@ EOF
 
 celebi-mv() {
 python - "$1" "$2" <<'EOF'
-from Chern.interface.shell import mv
+from CelebiChrono.interface.shell import mv
 import sys
 mv(sys.argv[1], sys.argv[2])
 EOF
@@ -49,7 +49,7 @@ EOF
 
 celebi-cp() {
 python - "$1" "$2" <<'EOF'
-from Chern.interface.shell import cp
+from CelebiChrono.interface.shell import cp
 import sys
 cp(sys.argv[1], sys.argv[2])
 EOF
@@ -57,7 +57,7 @@ EOF
 
 celebi-rm() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import rm
+from CelebiChrono.interface.shell import rm
 import sys
 rm(sys.argv[1])
 EOF
@@ -65,21 +65,21 @@ EOF
 
 celebi-short-ls() {
 python - <<'EOF'
-from Chern.interface.shell import short_ls
+from CelebiChrono.interface.shell import short_ls
 short_ls("")
 EOF
 }
 
 celebi-jobs() {
 python - <<'EOF'
-from Chern.interface.shell import jobs
+from CelebiChrono.interface.shell import jobs
 jobs("")
 EOF
 }
 
 celebi-mkalgorithm() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import mkalgorithm
+from CelebiChrono.interface.shell import mkalgorithm
 import sys
 mkalgorithm(sys.argv[1])
 EOF
@@ -87,7 +87,7 @@ EOF
 
 celebi-mktask() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import mktask
+from CelebiChrono.interface.shell import mktask
 import sys
 mktask(sys.argv[1])
 EOF
@@ -95,7 +95,7 @@ EOF
 
 celebi-mkdata() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import mkdata
+from CelebiChrono.interface.shell import mkdata
 import sys
 mkdata(sys.argv[1])
 EOF
@@ -103,7 +103,7 @@ EOF
 
 celebi-mkdir() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import mkdir
+from CelebiChrono.interface.shell import mkdir
 import sys
 mkdir(sys.argv[1])
 EOF
@@ -111,7 +111,7 @@ EOF
 
 celebi-rmfile() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import rm_file
+from CelebiChrono.interface.shell import rm_file
 import sys
 rm_file(sys.argv[1])
 EOF
@@ -119,7 +119,7 @@ EOF
 
 celebi-mvfile() {
 python - "$1" "$2" <<'EOF'
-from Chern.interface.shell import mv_file
+from CelebiChrono.interface.shell import mv_file
 import sys
 mv_file(sys.argv[1], sys.argv[2])
 EOF
@@ -127,7 +127,7 @@ EOF
 
 celebi-import() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import import_file
+from CelebiChrono.interface.shell import import_file
 import sys
 import_file(sys.argv[1])
 EOF
@@ -135,7 +135,7 @@ EOF
 
 celebi-add-input() {
 python - "$1" "$2" <<'EOF'
-from Chern.interface.shell import add_input
+from CelebiChrono.interface.shell import add_input
 import sys
 add_input(sys.argv[1], sys.argv[2])
 EOF
@@ -143,7 +143,7 @@ EOF
 
 celebi-remove-input() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import remove_input
+from CelebiChrono.interface.shell import remove_input
 import sys
 remove_input(sys.argv[1])
 EOF
@@ -151,7 +151,7 @@ EOF
 
 celebi-add-algorithm() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import add_algorithm
+from CelebiChrono.interface.shell import add_algorithm
 import sys
 add_algorithm(sys.argv[1])
 EOF
@@ -159,7 +159,7 @@ EOF
 
 celebi-add-parameter() {
 python - "$1" "$2" <<'EOF'
-from Chern.interface.shell import add_parameter
+from CelebiChrono.interface.shell import add_parameter
 import sys
 add_parameter(sys.argv[1], sys.argv[2])
 EOF
@@ -167,7 +167,7 @@ EOF
 
 celebi-rm-parameter() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import rm_parameter
+from CelebiChrono.interface.shell import rm_parameter
 import sys
 rm_parameter(sys.argv[1])
 EOF
@@ -175,7 +175,7 @@ EOF
 
 celebi-add-parameter-subtask() {
 python - "$1" "$2" "$3" <<'EOF'
-from Chern.interface.shell import add_parameter_subtask
+from CelebiChrono.interface.shell import add_parameter_subtask
 import sys
 add_parameter_subtask(sys.argv[1], sys.argv[2], sys.argv[3])
 EOF
@@ -183,7 +183,7 @@ EOF
 
 celebi-set-env() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import set_environment
+from CelebiChrono.interface.shell import set_environment
 import sys
 set_environment(sys.argv[1])
 EOF
@@ -191,7 +191,7 @@ EOF
 
 celebi-set-mem() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import set_memory_limit
+from CelebiChrono.interface.shell import set_memory_limit
 import sys
 set_memory_limit(sys.argv[1])
 EOF
@@ -199,14 +199,14 @@ EOF
 
 celebi-hosts() {
 python - <<'EOF'
-from Chern.interface.shell import hosts
+from CelebiChrono.interface.shell import hosts
 hosts()
 EOF
 }
 
 celebi-add-host() {
 python - "$1" "$2" <<'EOF'
-from Chern.interface.shell import add_host
+from CelebiChrono.interface.shell import add_host
 import sys
 add_host(sys.argv[1], sys.argv[2])
 EOF
@@ -214,14 +214,14 @@ EOF
 
 celebi-runners() {
 python - <<'EOF'
-from Chern.interface.shell import runners
+from CelebiChrono.interface.shell import runners
 runners()
 EOF
 }
 
 celebi-register-runner() {
 python - "$1" "$2" "$3" <<'EOF'
-from Chern.interface.shell import register_runner
+from CelebiChrono.interface.shell import register_runner
 import sys
 register_runner(sys.argv[1], sys.argv[2], sys.argv[3])
 EOF
@@ -229,7 +229,7 @@ EOF
 
 celebi-remove-runner() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import remove_runner
+from CelebiChrono.interface.shell import remove_runner
 import sys
 remove_runner(sys.argv[1])
 EOF
@@ -237,7 +237,7 @@ EOF
 
 celebi-send() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import send
+from CelebiChrono.interface.shell import send
 import sys
 send(sys.argv[1])
 EOF
@@ -245,7 +245,7 @@ EOF
 
 celebi-submit() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import submit
+from CelebiChrono.interface.shell import submit
 import sys
 runner = sys.argv[1] if len(sys.argv) > 1 else "local"
 submit(runner)
@@ -254,7 +254,7 @@ EOF
 
 celebi-view() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import view
+from CelebiChrono.interface.shell import view
 import sys
 browser = sys.argv[1] if len(sys.argv) > 1 else "open"
 view(browser)
@@ -263,7 +263,7 @@ EOF
 
 celebi-edit() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import edit_script
+from CelebiChrono.interface.shell import edit_script
 import sys
 edit_script(sys.argv[1])
 EOF
@@ -271,14 +271,14 @@ EOF
 
 celebi-config() {
 python - <<'EOF'
-from Chern.interface.shell import config
+from CelebiChrono.interface.shell import config
 config()
 EOF
 }
 
 celebi-danger() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import danger_call
+from CelebiChrono.interface.shell import danger_call
 import sys
 danger_call(sys.argv[1])
 EOF
@@ -286,7 +286,7 @@ EOF
 
 celebi-trace() {
 python - "$1" <<'EOF'
-from Chern.interface.shell import trace
+from CelebiChrono.interface.shell import trace
 import sys
 trace(sys.argv[1])
 EOF
@@ -294,14 +294,14 @@ EOF
 
 celebi-history() {
 python - <<'EOF'
-from Chern.interface.shell import history
+from CelebiChrono.interface.shell import history
 print(history().colored())
 EOF
 }
 
 celebi-changes() {
 python - <<'EOF'
-from Chern.interface.shell import changes
+from CelebiChrono.interface.shell import changes
 print(changes().colored())
 EOF
 }
@@ -309,14 +309,14 @@ EOF
 
 celebi-preshell() {
 python - <<'EOF'
-from Chern.interface.shell import workaround_preshell
+from CelebiChrono.interface.shell import workaround_preshell
 print(workaround_preshell())
 EOF
 }
 
 celebi-postshell() {
 python - <<'EOF'
-from Chern.interface.shell import workaround_postshell
+from CelebiChrono.interface.shell import workaround_postshell
 workaround_postshell()
 EOF
 }
